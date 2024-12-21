b6d0fc353a347ef11d47a7124ce8b48636210b9f
kengz <kengzwl@gmail.com>
2024-12-21 16:35:28 -0500

    working pattern

--- examples/nb.py
+++ examples/nb.py
@@ -95,8 +95,8 @@ graph_module
 x = torch.tensor([1.0])  # Input tensor
 outputs = graph_module(x)
 outputs
-print(f"B's Output: {outputs['A']}")
-print(f"C's Output: {outputs['B']}")
+print(f"B's Output: {outputs['B']}")
+print(f"C's Output: {outputs['C']}")
 
 
 # use this to redo the MLP example above
@@ -130,4 +130,118 @@ gm
 # test
 x = torch.randn(128)
 y = gm(x)
+y
+
+
+class ConcatMerge(nn.Module):
+    '''Merge layer to merge a dict of tensors by concatenating along dim=1. Reverse of Split'''
+    def __init__(self, dim: int = 0):
+        super().__init__()
+        self.dim = dim
+
+    def forward(self, *args: torch.Tensor) -> torch.Tensor:
+        return torch.cat(args, dim=self.dim)
+
+# register in torch.nn
+nn.ConcatMerge = ConcatMerge
+
+spec_yaml = '''
+modules:
+  my_mlp:
+    Sequential:
+        - Linear:
+            in_features: 128
+            out_features: 64
+        - ReLU: {}
+        - Linear:
+            in_features: 64
+            out_features: 10
+  my_mlp2:
+    Sequential:
+        - Linear:
+            in_features: 128
+            out_features: 64
+        - ReLU: {}
+        - Linear:
+            in_features: 64
+            out_features: 10
+  concat12:
+    ConcatMerge:
+        dim: 1
+  # concat 2, forward to 3
+  my_mlp3:
+    Linear:
+        in_features: 20
+        out_features: 10
+
+graph:
+  inputs: [x1, x2]
+  modules:
+    my_mlp:
+        args: [x1]
+    my_mlp2:
+        args: [x2]
+    concat12:
+        args: [my_mlp, my_mlp2]
+    my_mlp3:
+        args: [concat12]
+  output: [my_mlp2]
+'''
+
+spec = yaml.safe_load(spec_yaml)
+
+
+def build_sequential(module_specs: list[dict]) -> nn.Sequential:
+    module = nn.Sequential()
+    for module_spec in module_specs:
+        module_name, kwargs = next(iter(module_spec.items()))
+        cls = getattr(nn, module_name)
+        layer = cls(**kwargs)
+        module.append(layer)
+    return module
+
+
+# should only be sequential
+# create the modules
+modules = {}
+for name, module_spec in spec['modules'].items():
+    # module is always single-key dict
+    module_name = next(iter(module_spec))
+    cls = getattr(nn, module_name)
+    if cls == nn.Sequential:
+        module = build_sequential(module_spec[module_name])
+    else:
+        module = cls(**module_spec[module_name])
+    modules[name] = module
+
+modules
+
+graph = Graph()
+nodes = {}
+# first, create inputs
+for input_name in spec['graph']['inputs']:
+    nodes[input_name] = graph.placeholder(input_name)
+
+# next, create the intermediate nodes
+for name, subspec in spec['graph']['modules'].items():
+    # spec = {args: [x2]}
+    args = tuple([nodes[arg] for arg in subspec['args']])
+    node = graph.call_module(name, args)
+    nodes[name] = node
+
+# finally, create the output nodes
+# TODO args or dict
+graph.output([nodes[name] for name in spec['graph']['output']])
+
+graph.lint()
+graph.print_tabular()
+gm = fx.GraphModule(modules, graph)
+gm
+
+# hmm ok let's avoid functional altogether. purely module only
+# test
+x1 = torch.randn(4, 128)
+x2 = torch.randn(4, 128)
+y = gm(x1=x1, x2=x2)
+
 y
