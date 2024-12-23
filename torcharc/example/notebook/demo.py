b4a35c1a8746ce5827c6829bbdc329c6a5da795b
kengz <kengzwl@gmail.com>
2024-12-23 12:14:25 -0500

    refactor

--- torcharc/example/notebook/demo.py
+++ torcharc/example/notebook/demo.py
@@ -2,22 +2,22 @@ import torch
 import yaml
 import torcharc
 
-spec = yaml.safe_load(open("torcharc/specs/mlp.yaml"))
+spec = yaml.safe_load(open("torcharc/example/spec/mlp.yaml"))
 gm = torcharc.build(spec)
 print(gm.code)
 x = torch.randn(1, 128)
 y = gm(x)
 y
 
-spec = yaml.safe_load(open("torcharc/specs/mlp_lazy.yaml"))
-spec = yaml.safe_load(open("torcharc/specs/fork/chunk.yaml"))
-spec = yaml.safe_load(open("torcharc/specs/fork/split.yaml"))
+spec = yaml.safe_load(open("torcharc/example/spec/mlp_lazy.yaml"))
+spec = yaml.safe_load(open("torcharc/example/spec/fork/chunk.yaml"))
+spec = yaml.safe_load(open("torcharc/example/spec/fork/split.yaml"))
 gm = torcharc.build(spec)
 print(gm.code)
 x = torch.randn(1, 32)
 y = gm(x)
 y
-spec = yaml.safe_load(open("torcharc/specs/transformer.yaml"))
+spec = yaml.safe_load(open("torcharc/example/spec/transformer.yaml"))
 gm = torcharc.build(spec)
 batch_size = 4
 seq_len = 10
@@ -26,7 +26,7 @@ tgt_x = torch.randn(seq_len, batch_size, 10)
 y = gm(src_x=src_x, tgt_x=tgt_x)
 y.shape
 
-spec = yaml.safe_load(open("torcharc/specs/attention.yaml"))
+spec = yaml.safe_load(open("torcharc/example/spec/attention.yaml"))
 gm = torcharc.build(spec)
 batch_size = 4
 seq_len = 10
